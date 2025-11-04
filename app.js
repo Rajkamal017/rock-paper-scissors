232eaca25cbbdd7870093bf388b884542ed3b087
Raj Kamal <rajkamaldhan@gmail.com>
2025-11-04 14:48:11 +0530

    showWinner arguments was not completely wriiten

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ const playGame = (userChoice) => {
             userWin = compChoice === "rock" ? false :  true;
         }
 
-        showWinner(userWin);
+        showWinner(userWin, userChoice, compChoice);
     }
 };
 
